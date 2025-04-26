c5190009b45b2613bde70dbf7412a2b08773c154
AndreanIvan <andreanivanardianto@gmail.com>
2025-04-26 15:18:13 +0700

    Remove build folder

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/d/Programming/PkcsProject/pkcs11_solid
-# Build directory: /mnt/d/Programming/PkcsProject/pkcs11_solid/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
